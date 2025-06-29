2e88ac4001bbabf6f5963a1365e0cc584a61945f
JovHariono <128022304+JovHariono@users.noreply.github.com>
2025-06-29 19:46:30 +0700

    131

---
+++ belajar-spring-webmvc/src/test/java/programmerzamannow/webmvc/controller/FormControllerTest.java
@@ -0,0 +1,37 @@
+package programmerzamannow.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.regex.Matcher;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FormControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void formHello() throws Exception {
+
+        mockMvc.perform(
+                post("/form/hello")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("name", "Eko")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Eko"))
+        );
+    }
+}
